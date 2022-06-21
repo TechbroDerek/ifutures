d6689ceb4a211431294bf87b5024745da762ea9b
TechbroDerek <git config --global user.email you@example.com>
2022-06-21 12:13:27 +0100

    made the navbar funtional

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".ri-menu-5-line").addEventListener("click", ()=>{
+    document.querySelector(".nav1items").style.right = "0"
+})
+
+document.querySelector(".ri-close-fill").addEventListener("click", ()=>{
+    document.querySelector(".nav1items").style.right = "-300px"
+})
